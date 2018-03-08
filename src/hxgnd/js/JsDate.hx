44ae2c1ff8eb39e759f4322aac65825aa3afc25a
terurou <terurou@denkiyagi.jp>
2018-03-08 14:56:47 +0900

    Add JsDate

---
+++ src/hxgnd/js/JsDate.hx
@@ -0,0 +1,21 @@
+package hxgnd.js;
+
+@:native("Date")
+extern class JsDate {
+    static function UTC(year: Int, month: Int, ?day: Int, ?hour: Int, ?minute: Int, ?second: Int, ?millisecond: Int): JsDate;
+    static function now(): Int;
+    static function parse(dateString: String): JsDate;
+
+    @:overload(function (value: Int): Void {})
+    @:overload(function (dateString: String): Void {})
+    @:overload(function (year: Int, month: Int, ?day: Int, ?hour: Int, ?minute: Int, ?second: Int, ?millisecond: Int): Void {})
+    function new();
+
+    // TODO add methods
+    
+    function valueOf(): Int;
+
+    public inline function toHaxeDate(): Date {
+        return cast this;
+    }
+}
