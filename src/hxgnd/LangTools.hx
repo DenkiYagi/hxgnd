b37daa3093af9f82210e9d6b560e46fd5369f6d7
terurou <terurou@denkiyagi.jp>
2018-04-04 16:35:13 +0900

    Add LangTools.same()

--- src/hxgnd/LangTools.hx
+++ src/hxgnd/LangTools.hx
@@ -1,5 +1,11 @@
 package hxgnd;
 
+import Type.ValueType;
+import haxe.io.Bytes;
+import haxe.Constraints.IMap;
+#if js
+import hxgnd.js.JsNative;
+#end
 #if macro
 import haxe.macro.Expr;
 import haxe.macro.Context;
@@ -10,7 +16,7 @@ using Lambda;
 class LangTools {
     public static inline function eq<T>(a: Null<T>, b: Null<T>): Bool {
         #if js
-        return untyped __strict_eq__(a, b);
+        return JsNative.strictEq(a, b);
         #else
         return a == b;
         #end
@@ -18,7 +24,7 @@ class LangTools {
 
     public static inline function neq<T>(a: Null<T>, b: Null<T>): Bool {
         #if js
-        return untyped __strict_neq__(a, b);
+        return JsNative.strictNeq(a, b);
         #else
         return a != b;
         #end
@@ -34,7 +40,7 @@ class LangTools {
 
     #if js
     public static inline function isUndefined<T>(a: Null<T>): Bool {
-        return eq(a, js.Lib.undefined);
+        return JsNative.strictEq(a, js.Lib.undefined);
     }
     #end
 
@@ -91,5 +97,164 @@ class LangTools {
         }
         block.push(macro ${ {expr: EObjectDecl(map.array()), pos: Context.currentPos()} });
         return macro $b{block};
+    }
+
+    public static function same(value1: Dynamic, value2: Dynamic): Bool {
+        #if js
+        if (eq(value1, value2) || isNull(value1) && isNull(value2)) return true;
+        #else
+        if (eq(value1, value2)) return true;
+        #end
+        
+        var stack = [new Tuple2(value1, value2)];
+        var loop = 0;
+        while (stack.length > 0 && loop++ < 20) {
+            var tuple = stack.pop();
+            var a = tuple.value1;
+            var b = tuple.value2;
+
+            #if js
+            if (eq(a, b) || isNull(a) && isNull(b)) continue;
+            if (nonJsObject(a) || nonJsObject(b)) return false;
+            #else
+            if (eq(a, b)) continue;
+            if (Std.is(a, String) || Std.is(b, String)) return false;
+            #end
+
+            switch (Type.typeof(a)) {
+                case TEnum(aEnum):
+                    switch (Type.typeof(b)) {
+                        case TEnum(bEnum) if (eq(aEnum, bEnum)):
+                            if (neq(Type.enumIndex(cast a), Type.enumIndex(cast b))) return false;
+                            var aParams = Type.enumParameters(cast a);
+                            var bParams = Type.enumParameters(cast b);
+                            var len = aParams.length;
+                            if (neq(len, bParams.length)) return false;
+                            for (i in 0...len) {
+                                stack.push(new Tuple2(aParams[i], bParams[i]));
+                            }
+                            continue;
+                        case _:
+                            return false;
+                    }
+                case TObject:
+                    switch (Type.typeof(b)) {
+                        case TObject:
+                        case _: return false;
+                    }
+                case TClass(aCls):
+                    switch (Type.typeof(b)) {
+                        case TClass(bCls) if (eq(aCls, bCls)):
+                            if (Std.is(a, Array)) {
+                                var aArray: Array<Dynamic> = cast a;
+                                var bArray: Array<Dynamic> = cast b;
+                                var len = aArray.length;
+                                if (neq(len, bArray.length)) return false;
+                                for (i in 0...len) {
+                                    stack.push(new Tuple2(aArray[i], bArray[i]));
+                                }
+                                continue;
+                            } else if (Std.is(a, Date)) {
+                                var aDate: Date = cast a;
+                                var bDate: Date = cast b;
+                                if (neq(aDate.getTime(), bDate.getTime())) return false;
+                                continue;
+                            } else if (Std.is(a, Bytes)) {
+                                var aBytes: Bytes = cast a;
+                                var bBytes: Bytes = cast b;
+                                if (neq(aBytes.length, bBytes.length)) return false;
+                                for (i in 0...aBytes.length) {
+                                    if (neq(aBytes.get(i), bBytes.get(i))) return false;
+                                }
+                                continue;
+                            } else if (Std.is(a, IMap)) {
+                                var aMap: IMap<Dynamic, Dynamic> = cast a;
+                                var bMap: IMap<Dynamic, Dynamic> = cast b;
+                                var keys = [for (k in aMap.keys()) k];
+                                if (neq(keys.length, [for (k in bMap.keys()) k].length)) return false;
+                                for (key in keys) {
+                                    stack.push(new Tuple2(aMap.get(key), bMap.get(key)));
+                                }
+                                continue;
+                            }
+                            // TODO custom class comparator
+                        case _:
+                            return false;
+                    }
+                case _:
+                    return false;
+            }
+
+            var keys = Reflect.fields(a);
+            var ignoreKeys = [];
+            if (isIterable(a)) {
+                if (!isIterable(b)) return false;
+                var aIt: Iterator<Dynamic> = cast a.iterator();
+                var bIt: Iterator<Dynamic> = cast b.iterator();
+                if (isIterator(aIt)) {
+                    if (!isIterator(bIt)) return false;
+                    while (aIt.hasNext()) {
+                        if (!bIt.hasNext()) return false;
+                        stack.push(new Tuple2(aIt.next(), bIt.next()));
+                    }
+                    ignoreKeys = ["iterator"];
+                }
+            } else if (isIterator(a)) {
+                if (!isIterator(b)) return false;
+                while (a.hasNext()) {
+                    if (!b.hasNext()) return false;
+                    stack.push(new Tuple2(a.next(), b.next()));
+                }
+                ignoreKeys = ["hasNext", "next"];
+            }
+            if (neq(keys.length, Reflect.fields(b).length)) return false;
+            for (k in ignoreKeys) keys.remove(k);
+            for (key in keys) {
+                if (!Reflect.hasField(b, key)) return false;
+                stack.push(new Tuple2(Reflect.field(a, key), Reflect.field(b, key)));
+            }
+        }
+        
+        return true;
+    }
+
+    #if js
+    static inline function nonJsObject(x: Dynamic): Bool {
+        return JsNative.strictEq(x, null) || JsNative.strictNeq(JsNative.typeof(x), "object"); 
+    }
+    #end
+
+    static function isIterable(x: Dynamic): Bool {
+        return Reflect.isFunction(Reflect.field(x, "iterator"));
+    }
+
+    static inline function isSameIterable(a: Iterable<Dynamic>, b: Iterable<Dynamic>): Bool {
+        try {
+            var aIt: Iterator<Dynamic> = cast a.iterator();
+            var bIt: Iterator<Dynamic> = cast b.iterator();
+            while (aIt.hasNext()) {
+                if (!bIt.hasNext()) return false;
+                if (neq(aIt.next(), bIt.next())) return false;
+            }
+            return true;
+        } catch (e: Dynamic) {
+            return false;
+        }
+    }
+
+    static inline function isIterator(x : Dynamic) {
+        return Reflect.isFunction(Reflect.field(x, "next")) && Reflect.isFunction(Reflect.field(x, "hasNext"));
+    }
+
+    static inline function isSameIterator(a: Iterator<Dynamic>, b: Iterator<Dynamic>): Bool {
+        try {
+            while (a.hasNext()) {
+                if (!b.hasNext()) return false;
+                if (neq(a.next(), b.next())) return false;
+            }
+            return true;
+        } catch (e: Dynamic) {
+            return false;
+        }
     }
 }
