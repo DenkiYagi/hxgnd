e3b7e14b58ac842d06984da60c27e2fb1fd78400
terurou <terurou@denkiyagi.jp>
2018-04-15 19:00:51 +0900

    Add Unit._

--- src/hxgnd/Unit.hx
+++ src/hxgnd/Unit.hx
@@ -4,4 +4,7 @@ abstract Unit(Dynamic) {
     public inline function new() {
         this = null;
     }
+
+    public static var _(get, never): Unit;
+    static inline function get__() return new Unit();
 }
