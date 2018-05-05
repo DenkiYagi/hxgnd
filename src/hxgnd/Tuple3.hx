325c48fd3b2d7dbd414e5c2599eadbf604a58faa
terurou <terurou@denkiyagi.jp>
2018-05-05 22:59:16 +0900

    Add Tuple3-8

--- src/hxgnd/Tuple3.hx
+++ src/hxgnd/Tuple3.hx
@@ -1,11 +1,12 @@
 package hxgnd;
 
-abstract Tuple2<T1, T2>(Array<Dynamic>) {
+abstract Tuple3<T1, T2, T3>(Array<Dynamic>) {
     public var value1(get, never): T1;
     public var value2(get, never): T2;
+    public var value3(get, never): T3;
 
-    public inline function new(v1: T1, v2: T2) {
-        this = [v1, v2];
+    public inline function new(v1: T1, v2: T2, v3: T3) {
+        this = [v1, v2, v3];
     }
 
     inline function get_value1(): T1 {
@@ -14,5 +15,9 @@ abstract Tuple2<T1, T2>(Array<Dynamic>) {
 
     inline function get_value2(): T2 {
         return this[1];
+    }
+
+    inline function get_value3(): T3 {
+        return this[2];
     }
 }
