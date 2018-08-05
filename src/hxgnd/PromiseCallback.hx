4ee1f7520f4a72dd89c51d0775b03459ac8ea29f
terurou <terurou@denkiyagi.jp>
2018-08-05 15:23:47 +0900

    cleanup

--- src/hxgnd/PromiseCallback.hx
+++ src/hxgnd/PromiseCallback.hx
@@ -1,17 +1,8 @@
 package hxgnd;
 
-import externtype.Mixed2;
-
-// typedef PromiseCallback<T, TOut> = Mixed2<
-//     //T -> IPromise<TOut>,
-//     T -> Promise<TOut>,
-//     T -> TOut
-// >;
 abstract PromiseCallback<T, TOut>(T -> Dynamic)
     from T -> TOut
     from T -> Promise<TOut>
     #if js from T -> js.Promise<TOut> #end
     to T -> Dynamic
-{
-
-}
+{}
