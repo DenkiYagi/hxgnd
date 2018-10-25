08a5b2ea6fd82ad2d2a4b1b641ca79db2a9075e4
terurou <terurou@denkiyagi.jp>
2018-10-25 11:42:59 +0900

    Suppress the unhandledRejection message on Node.js

--- test/TestMain.hx
+++ test/TestMain.hx
@@ -1,52 +1,80 @@
 package;
 
 import buddy.*;
-
-#if js
-class TestMain implements Buddy<[
-    // hxgnd.LangToolsTest,
-    // hxgnd.ArrayToolsTest,
-    // hxgnd.MaybeTest,
-    // hxgnd.DelegateTest,
-    // hxgnd.ComputationTest,
-    // hxgnd.StreamTest,
-    hxgnd.PromiseTest,
-    hxgnd.SyncPromiseTest,
-    // hxgnd.AbortablePromiseTest,
-    // hxgnd.PromiseToolsTest,
-    // hxgnd.ReactivePropertyTest,
-    // hxgnd.ReactiveActorTest,
-    hxgnd.ReactiveStreamTest,
-]> {}
-#else
 import buddy.reporting.ConsoleColorReporter;
 
+// please see https://github.com/ciscoheat/buddy/blob/master/src/buddy/internal/GenerateMain.hx
 class TestMain {
+    static var suites = [
+        // new hxgnd.LangToolsTest(),
+        // new hxgnd.ArrayToolsTest(),
+        // new hxgnd.MaybeTest(),
+        // new hxgnd.DelegateTest(),
+        // new hxgnd.ComputationTest(),
+        // // new hxgnd.StreamTest(),
+        new hxgnd.PromiseTest(),
+        new hxgnd.SyncPromiseTest(),
+        // new hxgnd.AbortablePromiseTest(),
+        // new hxgnd.PromiseToolsTest(),
+        // new hxgnd.ReactivePropertyTest(),
+        // new hxgnd.ReactiveActorTest(),
+        new hxgnd.ReactiveStreamTest(),
+    ];
+
     public static function main() {
+        var reporter = new ConsoleColorReporter();
+        var runner = new buddy.SuitesRunner(suites, reporter);
+        var oldTrace = haxe.Log.trace;
+
+        function outputError() {
+            haxe.Log.trace = oldTrace; // Restore original trace
+
+            var pos = {
+                fileName : "Buddy",
+                lineNumber : 0,
+                className : "",
+                methodName : ""
+            };
+
+            haxe.Log.trace(runner.unrecoverableError, pos);
+
+            var stack = runner.unrecoverableErrorStack;
+            if (stack == null || stack.length == 0) return;
+
+            for (s in stack) switch s {
+                case FilePos(_, file, line) if (line > 0):
+                    haxe.Log.trace(file + ":" + line, pos);
+                case _:
+            }
+        }
+
+        function startRun(done : Void -> Void) : Void {
+            // lua fix, needs temp var
+            var r = runner.run();
+            r.then(function(_) {
+                if (runner.unrecoverableError != null) outputError();
+                done();
+            });
+        }
+
+#if (js && nodejs)
+        untyped __js__("process.on('uncaughtException', {0})", function (err) {
+            runner.haveUnrecoverableError(err);
+        });
+        #if !showUnhandledRejection
+        untyped __js__("process.on('unhandledRejection', {0})", function (err) {});
+        #end
+        startRun(function () {
+            untyped __js__("process.exit({0})", runner.statusCode());
+        });
+#elseif (neko || cpp || python)
         haxe.EntryPoint.run();
         TestTools.run();
         promhx.base.EventLoop.nextLoop = haxe.EntryPoint.runInMainThread;
 
         var testsDone = false;
-        var origTrace = haxe.Log.trace;
-        var reporter = new ConsoleColorReporter();
-        var runner = new buddy.SuitesRunner([
-            // new hxgnd.LangToolsTest(),
-            // new hxgnd.ArrayToolsTest(),
-            // new hxgnd.MaybeTest(),
-            // new hxgnd.DelegateTest(),
-            // new hxgnd.ComputationTest(),
-            // // new hxgnd.StreamTest(),
-            new hxgnd.PromiseTest(),
-            new hxgnd.SyncPromiseTest(),
-            // new hxgnd.AbortablePromiseTest(),
-            // new hxgnd.PromiseToolsTest(),
-            // new hxgnd.ReactivePropertyTest(),
-            // new hxgnd.ReactiveActorTest(),
-            new hxgnd.ReactiveStreamTest(),
-        ], reporter);
         runner.run().then(function(_) {
-            if (runner.unrecoverableError != null) origTrace(runner.unrecoverableError);
+            if (runner.unrecoverableError != null) oldTrace(runner.unrecoverableError);
             testsDone = true;
         });
 
@@ -57,6 +85,8 @@ class TestMain {
                 Sys.exit(runner.statusCode());
             });
         });
+#else
+        startRun(function () {});
+#end
     }
 }
-#end
